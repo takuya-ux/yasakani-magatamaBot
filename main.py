72654064121b17b65c6528e52112be237f3d10f0
takuya-ux <1911140027r@ed.fuk.kindai.ac.jp>
2021-08-31 11:30:50 +0900

    とりあえず実験

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ import os
 import random
 import MeCab
 
+from wordcloud import WordCloud
+from PIL import Image
+import numpy as np
+
 app = Flask(__name__)
 
 #環境変数取得
@@ -49,10 +53,69 @@ def handle_message(event):
     #     TextSendMessage(text=message))
 
     mecab = MeCab.Tagger()
-    message = event.message.text
+    # message = event.message.text
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=mecab.parse(message)))
+
+    FONT_PATH = "ipaexg.ttf"
+    #####################################
+    TXT_NAME = "wakaDataSet"
+    #####################################
+
+    def get_word_str(text):
+        import MeCab
+        import re
+    
+        mecab = MeCab.Tagger()
+        parsed = mecab.parse(text)
+        lines = parsed.split('\n')
+        lines = lines[0:-2]
+        word_list = []
+    
+        for line in lines:
+            tmp = re.split('\t|,', line)
+    
+            # 名詞のみ対象
+            if tmp[1] in ["名詞"]:
+                # さらに絞り込み
+                if tmp[2] in ["一般", "固有名詞"]:
+                    word_list.append(tmp[0])
+    
+        return " " . join(word_list)
+    
+    #メッセージをテキストファイルに追加
+    post_text = event.event.message.text
+    # with open(TXT_NAME + ".txt", mode='a' , encoding="utf8") as f:
+    #     f.write("," + post_text)
+
+
+    # テキストファイル読み込み
+    read_text = open(TXT_NAME + ".txt", encoding="utf8").read()
+
+    ####################################################
+    # マスクを作成する
+    mask_array = np.array(Image.open('tree080948.png'))
+    #####################################################
+
+    # 文字列取得
+    word_str = get_word_str(read_text)
+    
+    # 画像作成
+    wc = WordCloud(font_path=FONT_PATH, mask=mask_array, background_color='white', colormap='bone', contour_width=3).generate(word_str)
+    
+    # 画像保存（テキストファイル名で）
+    # wc.to_file(TXT_NAME + ".png")
+
+    # PILで表示する
+    image_array = wc.to_array()
+    img = Image.fromarray(image_array)
+    img.to_file("/static/images" + TXT_NAME + ".png")
+
+
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=mecab.parse(message)))
+    event.reply_token,
+    TextSendMessage(text=mecab.parse(post_text)))
 
         
 
